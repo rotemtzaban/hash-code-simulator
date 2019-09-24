81b86c7d6e9bb59cc46f073a2539789071d25d9b
idanperez <ipdan96@gmail.com>
2019-09-24 22:36:23 +0300

    added menu

--- client/components/AppBar/index.tsx
+++ client/components/AppBar/index.tsx
@@ -8,14 +8,19 @@ import MenuIcon from '@material-ui/icons/Menu';
 import styles from './styles'
 import { WithStyles, withStyles } from '@material-ui/styles';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { MenuItem, Menu } from '@material-ui/core';
+import CloudUpload from '@material-ui/icons/CloudUpload';
+import Score from '@material-ui/icons/Score';
 
-interface AppBarProps extends WithStyles<typeof styles> , RouteComponentProps<any> {
-  isLoggedIn : boolean;
+
+interface AppBarProps extends WithStyles<typeof styles>, RouteComponentProps<any> {
+  isLoggedIn: boolean;
 }
 
 function ButtonAppBar(props: AppBarProps) {
-  const {classes} = props;
-  const toolbarTittle = props.isLoggedIn ?  "Hashcode" :"Sign in to start";
+  const { classes } = props;
+  const toolbarTittle = "Hashcode";
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -26,10 +31,27 @@ function ButtonAppBar(props: AppBarProps) {
           <Typography variant="h6" className={classes.title}>
             {toolbarTittle}
           </Typography>
-          <Button onClick={() => props.history.push("/signup")} color="inherit">Login</Button>
+          {props.isLoggedIn &&
+            < MenuItem onClick={() => props.history.push("/submit")}>
+              <IconButton aria-label="show 4 new mails" color="inherit">
+                <CloudUpload />
+              </IconButton>
+              <p>Submit Try</p>
+            </MenuItem >}
+          {props.isLoggedIn &&
+            < MenuItem onClick={() => props.history.push("/scoreboard")} >
+              <IconButton aria-label="show 4 new mails" color="inherit">
+                <Score />
+              </IconButton>
+              <p>Scoreboard</p>
+            </MenuItem >
+          }
+          {!props.isLoggedIn &&
+            <Button onClick={() => props.history.push("/signup")} color="inherit">Login</Button>
+          }
         </Toolbar>
       </AppBar>
-    </div>
+    </div >
   );
 }
 
