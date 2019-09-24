8daa5f3b8d247f7c8ec341029c82775a2853fd61
Rotem Tzaban <rotemtzaban@gmail.com>
2019-09-24 22:37:01 +0300

    added minimal scoreboard

---
+++ client/components/ScoreBoard/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { createStyles, Theme, withStyles } from '@material-ui/core';
+import { ThemeProvider, WithStyles } from '@material-ui/styles';
+
+const styles = (theme: Theme) =>
+    createStyles({
+        root: {
+            width: '40%',
+            marginTop: theme.spacing(3),
+            overflowX: 'auto',
+            overflowY: 'auto',
+        },
+        table: {
+            minWidth: 200
+        }
+    });
+
+interface TeamRecord {
+    team: string;
+    score: number;
+    position: number;
+}
+
+const data: TeamRecord[] = [
+    { team: 'pixel', score: 9001, position: 1 },
+    { team: 'anotherTeam', score: 8000, position: 2 },
+    { team: 'anotherTeam2', score: 7000, position: 3 }
+];
+
+interface ScoreBoardProps extends WithStyles<typeof styles> {}
+
+class ScoreBoard extends React.Component<ScoreBoardProps> {
+    render() {
+        const classes = this.props.classes;
+        return (
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">Team</TableCell>
+                            <TableCell align="left">Score</TableCell>
+                            <TableCell align="left">Position</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((row, i) => (
+                            <TableRow key={row.team}>
+                                <TableCell align="left">{row.team}</TableCell>
+                                <TableCell align="left">{row.score}</TableCell>
+                                <TableCell align="left">
+                                    {row.position}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
+}
+
+export default withStyles(styles)(ScoreBoard);
